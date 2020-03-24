b38294638c7386d04e1d1220da70f0d763911643
Thierry Martinez <Thierry.Martinez@inria.fr>
2020-03-25 00:59:35 +0100

    Install git

--- docker/configure_environment.sh
+++ docker/configure_environment.sh
@@ -2,4 +2,4 @@
 set -ex
 
 sudo apt-get install --yes \
-  build-essential gfortran unzip m4 bison flex patch autoconf automake
+  build-essential gfortran unzip m4 bison flex patch autoconf automake git
