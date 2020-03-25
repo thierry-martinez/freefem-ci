d76bd673e9c85fc92d29a50712df9f29974d5af1
Thierry Martinez <Thierry.Martinez@inria.fr>
2020-03-25 02:35:43 +0100

    Install cmake

--- docker/configure_environment.sh
+++ docker/configure_environment.sh
@@ -3,4 +3,4 @@ set -ex
 
 sudo apt-get install --yes \
   build-essential gfortran unzip m4 bison flex patch autoconf automake git \
-  wget file python2.7 mpi-default-dev
+  wget file python2.7 mpi-default-dev cmake
