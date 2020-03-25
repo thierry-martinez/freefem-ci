b4116396cbcfeb73d06ff504958996c4768767c8
Thierry Martinez <Thierry.Martinez@inria.fr>
2020-03-25 10:51:05 +0100

    Install python

--- configure_environment.sh
+++ configure_environment.sh
@@ -3,4 +3,4 @@ set -ex
 
 sudo apt-get install --yes \
   build-essential gfortran unzip m4 bison flex patch autoconf automake git \
-  wget file python2.7 mpi-default-dev cmake
+  wget file python2.7 python mpi-default-dev cmake
