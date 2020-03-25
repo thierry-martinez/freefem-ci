6eccb673e7e39f9009c9e21d6841cb0c730ffc71
Thierry Martinez <Thierry.Martinez@inria.fr>
2020-03-25 10:07:14 +0100

    Try to install in a prefix

--- build_freefem.sh
+++ build_freefem.sh
@@ -3,8 +3,8 @@ set -ex
 
 cd FreeFem-sources
 autoreconf -i
-./configure --enable-download --enable-optim
+./configure --enable-download --enable-optim --prefix=$HOME/freefem
 3rdparty/getall -a
-make -C 3rdparty/ff-petsc petsc-slepc SUDO=sudo
+make -C 3rdparty/ff-petsc petsc-slepc
 ./reconfigure
 make -j 2
