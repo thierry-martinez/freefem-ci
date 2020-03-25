480f4f698809659b78ccb475fd976467412d6c40
Thierry Martinez <Thierry.Martinez@inria.fr>
2020-03-25 02:46:47 +0100

    Specify SUDO_USER

--- docker/build_freefem.sh
+++ docker/build_freefem.sh
@@ -5,6 +5,6 @@ cd FreeFem-sources
 autoreconf -i
 ./configure --enable-download --enable-optim
 3rdparty/getall -a
-make -C 3rdparty/ff-petsc petsc-slepc SUDO=sudo
+make -C 3rdparty/ff-petsc petsc-slepc SUDO=sudo SUDO_USER=root
 ./reconfigure
 make -j 2
